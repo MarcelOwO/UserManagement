4a179cbf7913b23a07ad02641eb944381dff7115
Marcel <Marcel-sigl@gmx.de>
2025-11-25 21:54:58 +0100

    setup apphost with projects

--- UserManagement.AppHost/AppHost.cs
+++ UserManagement.AppHost/AppHost.cs
@@ -1,3 +1,23 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var sql = builder.AddSqlServer("sql")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var userDb = sql.AddDatabase("UserDb");
+
+var groupDb = sql.AddDatabase("GroupDb");
+
+var api = builder.AddProject<UserManagement_Api>("Api")
+    .WithReference(userDb)
+    .WithReference(groupDb)
+    .WaitFor(userDb)
+    .WaitFor(groupDb);
+
+var web =  builder.AddProject<UserManagement_Web>("Web")
+    .WithExternalHttpEndpoints()
+    .WithReference(api)
+    .WaitFor(api);
 
 builder.Build().Run();
